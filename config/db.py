8e891fc97f77491ee663fa022721c63f7937b02a
leonvic13 <leonvic13@gmail.com>
2026-03-30 14:51:00 -0600

    feat: add dev/stage/prod environment setup

--- config/db.py
+++ config/db.py
@@ -11,11 +11,11 @@ SQLITE = {
 
 POSTGRESQL = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'leon',
-        'USER': 'postgres',
-        'PASSWORD': 'a125%gofP',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
