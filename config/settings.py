957591139c8f5531c2f23cf7244e4299547567d9
leonvic13 <leonvic13@gmail.com>
2026-03-30 15:07:55 -0600

    fix: use CompressedStaticFilesStorage to avoid missing sourcemap error

--- config/settings.py
+++ config/settings.py
@@ -102,7 +102,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
